41c131061a411f49a9f9f4646b7b976e59bda8c1
Ninad Deodhare <2025.ninadd@isu.ac.in>
2026-09-22 12:03:18 +0530

    Fix Render port configuration

--- Ninad 150096725180/server.js
+++ Ninad 150096725180/server.js
@@ -30,9 +30,9 @@ const isAuthenticated = passport.authenticate('local', { session: false });
 
 app.use('/', authRouter);
 app.use('/hospitals', isAuthenticated, hospitalRouter);
+const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
 module.exports = app;
